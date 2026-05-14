1dc3c59eb5e47d6371764574eb6fff2ea86d0e15
Samuel Baafi Boakye <samuelbaafiboakye@gmail.com>
2026-05-14 16:31:35 +0200

    add logging to db call

--- module/core/src/main/scala/com/sbboakye/masel/core/ports/Repositories.scala
+++ module/core/src/main/scala/com/sbboakye/masel/core/ports/Repositories.scala
@@ -28,5 +28,4 @@ trait AppDb[F[_]: MonadThrow]:
   def withSession[A](use: Repos[F] => F[A]): F[A]
   def withTransaction[A](use: Repos[F] => F[A]): F[A]
   def isReady: F[Boolean]
-  def dbCall[A](fa: F[A]): F[A] =
-    fa.adaptError { case e => AppError.DatabaseError }
+  def dbCall[A](fa: F[A]): F[A]
